bd760b80e0e6ea6c4487334112c18886b804eb33
zalakdelvadiya <30698566+zalakdelvadiya@users.noreply.github.com>
2017-08-14 22:38:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/artical-one',function(req,res){
 });
 
 app.get('/artical-two',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
 });
 
 app.get('/artical-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
