428fddc936dde0d2df139a051be22888e760f5d4
zalakdelvadiya <30698566+zalakdelvadiya@users.noreply.github.com>
2017-08-14 23:46:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articalOne={
+    title:'Artical one | zalak delvadiya';
+    heading:'Artical one';
+    date:'aug 14,2017
+    content:'
+            <p>
+                this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.
+            </p>
+            <p>
+                this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.
+            </p>
+            <p>
+                this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.
+            </p>'
+            
+};
+var htmlTemplete={
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        
+        
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        <h3>
+            
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            
+            ${content}
+            
+        </div>
+        </div>
+    </body>
+    
+</html>
+
+    
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
