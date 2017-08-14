4fb0bb53603f198ab68c8a09475f9c83a7cf1225
zalakdelvadiya <30698566+zalakdelvadiya@users.noreply.github.com>
2017-08-14 22:04:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one',function(req,res){
+    res.send('Artical one requested and will be served here');
+});
+
+app.get('/artical-two',function(req,res){
+    res.send('Artical two requested and will be served here');
+});
+
+app.get('/artical-three',function(req,res){
+    res.send('Artical three requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
