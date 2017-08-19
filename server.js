ea4193c349d64f5fe197816bc0bf70f5e24d23e2
zalakdelvadiya <30698566+zalakdelvadiya@users.noreply.github.com>
2017-08-19 22:37:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,11 @@ var htmlTemplete=`
     return htmlTemplete;
 }
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter,toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
