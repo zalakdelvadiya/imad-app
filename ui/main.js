359774597236c692d5ec66c3c7084e364b28a175
zalakdelvadiya <30698566+zalakdelvadiya@users.noreply.github.com>
2017-08-19 23:42:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 var button=document.getElementById('counter');
-var counter=0;
+
+
 
 button.onclick=function(){
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
-};
+    
+    var request=new XMLhttpRequest();
+     
+     request.onreadystatechange=function(){
+     if(request.readyState===XMLhttpRequest.DONE)
+     {
+         if(request.state===200){
+           var counter= request.responseText;
+            var span=document.getElementById('count');
+             span.innerHTML=counter.toString();
+         }
+     }
+     };
+     request.open('GET','http://zalakdelvadiya.imad.hasurta-app.io/counter',true);
+     request.send(null);
+  };
