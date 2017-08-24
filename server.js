124e6e082b37f5b97038604115eabbc8ae42fe80
zalakdelvadiya <30698566+zalakdelvadiya@users.noreply.github.com>
2017-08-24 08:08:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config={
+
+    user:'zalakddelvadiya',
+    database:'zalakddelvadiya',
+    host:'db.imad.hasura3-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,6 +83,19 @@ app.get('/counter',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool =new pool(config);
+app.get('test-db',function(req,res){
+    pool.query("SELECT * FROM test",function(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        }
+        else{
+            res.send(JSON.string)
+        }
+    })
+});
+
 
 app.get('/artical-one',function(req,res){
     res.send(createTemplete(articalOne));
